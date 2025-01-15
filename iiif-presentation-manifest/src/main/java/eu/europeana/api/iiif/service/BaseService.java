0d40b72c6ab9b33a274275f603044782329611dd
srishti <srishti.singh@europeana.eu>
2025-01-15 10:21:48 +0100

    EA-4018 fix all the errors

--- iiif-presentation-manifest/src/main/java/eu/europeana/api/iiif/service/BaseService.java
+++ iiif-presentation-manifest/src/main/java/eu/europeana/api/iiif/service/BaseService.java
@@ -3,7 +3,6 @@ package eu.europeana.api.iiif.service;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.europeana.api.iiif.connection.HttpConnection;
 
@@ -16,7 +15,6 @@ public class BaseService {
         httpConnection = new HttpConnection();
         mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
     }
